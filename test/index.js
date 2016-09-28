0fafd64821790950703cc882a745628e1b60b640
Daniel Cousens <github@dcousens.com>
2016-09-28 20:45:28 +1000

    tests: revert to ES5 until 0.12 dropped

--- test/index.js
+++ test/index.js
@@ -3,48 +3,54 @@ var fixtures = require('./fixtures')
 var test = require('tape')
 
 function augment (array) {
-  return array.map((x, i) => Object.assign({}, x, { i }))
+  return array.map(function (x, i) {
+    return Object.assign({}, x, { i })
+  })
 }
 
 function fInputs (inputs) {
-  return inputs.map(x => Object.assign({}, x, {
-    txId: [].reverse.call(new Buffer(x.txId, 'hex'))
-  }))
+  return inputs.map(function (x) {
+    return Object.assign({}, x, {
+      txId: [].reverse.call(new Buffer(x.txId, 'hex'))
+    })
+  })
 }
 
 function fOutputs (outputs) {
-  return outputs.map(x => Object.assign({}, x, {
-    script: new Buffer(x.script, 'hex')
-  }))
+  return outputs.map(function (x) {
+    return Object.assign({}, x, {
+      script: new Buffer(x.script, 'hex')
+    })
+  })
 }
 
-fixtures.inputs.forEach((f) => {
-  test('sortInputs ' + f.description, (t) => {
+fixtures.inputs.forEach(function (f) {
+  test('sortInputs ' + f.description, function (t) {
     t.plan(1)
 
     var inputs = augment(f.inputs)
     var actual = bip69.sortInputs(inputs)
 
-    t.same(actual.map(x => x.i), f.expected)
+    t.same(actual.map(function (x) { return x.i }), f.expected)
   })
 
-  test('sortInputs (w/ txHash Buffers) ' + f.description, (t) => {
+  test('sortInputs (w/ txHash Buffers) ' + f.description, function (t) {
     t.plan(1)
 
     var inputs = augment(fInputs(f.inputs))
     var actual = bip69.sortInputs(inputs)
 
-    t.same(actual.map(x => x.i), f.expected)
+    t.same(actual.map(function (x) { return x.i }), f.expected)
   })
 })
 
-fixtures.outputs.forEach((f) => {
-  test('sortOutputs ' + f.description, (t) => {
+fixtures.outputs.forEach(function (f) {
+  test('sortOutputs ' + f.description, function (t) {
     t.plan(1)
 
     var outputs = augment(fOutputs(f.outputs))
     var actual = bip69.sortOutputs(outputs)
 
-    t.same(actual.map(x => x.i), f.expected)
+    t.same(actual.map(function (x) { return x.i }), f.expected)
   })
 })
